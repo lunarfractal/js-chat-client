9b929474ccde9fa2514039d25aa686a95be74795
01060c <xxs239239@gmail.com>
2025-06-01 15:05:02 +0530

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,12 +1,15 @@
 #include <websocketpp/client.hpp>
 #include <websocketpp/config/asio_client.hpp>
 
+#include <unistd.h>
+#include <sys/ioctl.h>
+
 #include <nlohmann/json.hpp>
 
-const std::string COLOR_RESET     = "\033[0m";
-const std::string COLOR_BLUE  = "\033[1;34m";
-const std::string COLOR_WHITE      = "\033[0;37m";
-const std::string COLOR_GREY = "\033[90m";
+#define COLOR_BLUE  "\033[34m"
+#define COLOR_WHITE "\033[37m"
+#define COLOR_GREY  "\033[90m"
+#define COLOR_RESET "\033[0m"
 
 typedef websocketpp::client<websocketpp::config::asio_tls_client> client;
 typedef websocketpp::connection_hdl connection_hdl;
@@ -17,18 +20,28 @@ tyepdef websocketpp::lib::asio::ssl::context ssl_context;
 using websocketpp::lib::placeholders::_1;
 using websocketpp::lib::placeholders::_2;
 
-void print_message(std::string &username, std::string &text, std::string &timestamp) {
-  std::string half = COLOR_BLUE + username + ": " + COLOR_WHITE + text + COLOR_GREY + "   [" + timestamp + "]" + COLOR_RESET;
-  std::cout << half << std::endl
+void print_message(std::string &username, std::string &text, std::string &timestamp) 
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    int term_width = w.ws_col;
+
+    std::string user_text = COLOR_BLUE + username + ": " + COLOR_WHITE + text + COLOR_RESET;
+    std::string time_text = COLOR_GREY + "[" + timestamp + "]" + COLOR_RESET;
+
+    int spacing = term_width - (int)(user_text.length() + time_text.length());
+    if (spacing < 1) spacing = 1;
+
+    std::string spaces(spacing, ' ');
+
+    std::cout << user_text << spaces << time_text << std::endl;
 }
 
-
-void send_text_message(Client* client, ConnectionHdl* connection, std::string msg) {
-  client->send(*connection, msg, websocketpp::frame::opcode::text);
+void send_text_message(client* c, connection_hdl* connection, std::string msg) {
+  c->send(*connection, msg, websocketpp::frame::opcode::text);
 }
 
 void on_message(client* client, connection_hdl hdl, message_ptr msg) {
-  nholmann::json data = nholmann::json::parse(msg->get_payload());
+  nlohmann::json data = nlohmann::json::parse(msg->get_payload());
   std::string type = data["type"];
   switch(type) {
     case "chat":
@@ -120,7 +133,7 @@ int main(int argc, char* argv[]) {
       done = true;
       c.close(hdl, websocketpp::close::status::normal, "done");
     } else {
-
+      send_text_message(&c, &hdl, "{\"type\":\"message\",\"content\":\"" + input + "\"}";
     }
   }
 
